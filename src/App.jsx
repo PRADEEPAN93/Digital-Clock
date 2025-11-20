f1ae9391e8a126e955cb0e55e457bc71d486d7e7
PRADEEPAN93 <pradeepank6070@gmail.com>
2025-11-20 17:13:34 +0530

    Digital Clock

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import './App.css'
+import { useEffect } from 'react';
+
+function App() {
+
+  const [currentTime, setCurrentTime] = useState(new Date());
+ 
+  useEffect(() =>{
+    const timer = setInterval(()=>{
+      setCurrentTime(new Date());
+    },1000);
+    return() => clearInterval(timer);
+  }, []);
+
+  //Railway time to normal time
+  const formatHour = (hour) =>{
+    return hour === 0 ? 12 : hour > 12 ? hour - 12 : hour;
+  };
+// Single digit number add 0
+  const formatTimeWithLeadingZero = (num) =>{
+    return num < 10 ? `0${num}` : num;
+  };
+
+  //Date
+  const formatDate = (date) => {
+    const options = {weekday :  "long" , year: "numeric", month: "long", day:"numeric"};
+    return date.toLocaleDateString(undefined, options);
+  };
+  return (
+    <>
+    <div className="digital-clock">
+      <h1>Digital Clock</h1>
+      <div className="time">
+        { formatTimeWithLeadingZero(formatHour(currentTime.getHours()))}: 
+        {formatTimeWithLeadingZero(currentTime.getMinutes())}:
+        {formatTimeWithLeadingZero(currentTime.getSeconds())}
+        {currentTime.getHours() >= 12 ? " PM" : " AM"}
+        </div>
+      <div className="date">{formatDate(currentTime)}</div>
+    </div>
+
+    </>
+  );
+};
+
+export default App;
+
+
+
+
+//git remote add origin https://github.com/pradeepan93/Digital Clock
